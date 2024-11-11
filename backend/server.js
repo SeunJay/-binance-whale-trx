6d32ef7c2c82fcaff14118e1569e73c3b46ae1fd
Oluwaseun Jayeoba <seunjay92@gmail.com>
2024-11-11 17:09:31 +0100

    implement service to fetch transactions from the bit query API

--- backend/server.js
+++ backend/server.js
@@ -86,7 +86,7 @@ app.use(function (err, req, res, next) {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6001;
 
 server.listen(PORT, function() {
     console.log(`Server running on PORT ${PORT}`);
---
+++ backend/services/TransactionServices.js
@@ -0,0 +1,57 @@
+const axios = require("axios");
+require("dotenv").config();
+
+const bitQueryUrl = require("../config/key").bitQueryURL;
+const bitQueryApiKey = require("../config/key").bitQueryApiKey;
+
+exports.fetchTransactions = async () => {
+  const query = `
+      query {
+        ethereum(network: bsc) {
+            transactions(options: {asc: "block.timestamp.time", limit: 10}) {
+            amount(date: {since: null, till: null})
+            sender(txSender: {in: "0xF6dDc8aCAFdA5BB95271CB73CFDe3AB7f67d3c99"}) {
+             address
+            }
+             gasValue
+             hash
+             currency {
+                symbol
+                address
+                name
+             }
+             index
+            to {
+            address
+            smartContract {
+                contractType
+                currency {
+                 name
+                tokenType
+                symbol
+            }
+           }
+        }
+        block {
+        height
+        timestamp {
+            time
+        }
+        }
+    }
+    }
+      }
+    `;
+
+  try {
+    const response = await axios.post(
+      bitQueryUrl,
+      { query },
+      { headers: { "X-API-KEY": bitQueryApiKey } }
+    );
+
+    return response.data.data.ethereum.transactions;
+  } catch (error) {
+    throw new Error(`Error fetching transactions: ${error.message}`);
+  }
+};
